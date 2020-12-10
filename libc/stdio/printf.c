a698d315da7bdda3223d9b9718f4919e04d5e09b
Ivan Rekunov <vanno6521@gmail.com>
2020-12-10 17:27:49 +0400

    Delete some useless code from printf()

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -41,40 +41,6 @@ int printf(const char* restrict format, ...) {
 
 		const char* format_begun_at = format++;
 
-		// if (*format == 'c') {
-		// 	format++;
-		// 	char c = (char) va_arg(parameters, int /* char promotes to int */);
-		// 	if (!maxrem) {
-		// 		// TODO: Set errno to EOVERFLOW.
-		// 		return -1;
-		// 	}
-		// 	if (!print(&c, sizeof(c)))
-		// 		return -1;
-		// 	written++;
-		// } else if (*format == 's') {
-		// 	format++;
-		// 	const char* str = va_arg(parameters, const char*);
-		// 	size_t len = strlen(str);
-		// 	if (maxrem < len) {
-		// 		// TODO: Set errno to EOVERFLOW.
-		// 		return -1;
-		// 	}
-		// 	if (!print(str, len))
-		// 		return -1;
-		// 	written += len;
-		// } else {
-		// 	format = format_begun_at;
-		// 	size_t len = strlen(format);
-		// 	if (maxrem < len) {
-		// 		// TODO: Set errno to EOVERFLOW.
-		// 		return -1;
-		// 	}
-		// 	if (!print(format, len))
-		// 		return -1;
-		// 	written += len;
-		// 	format += len;
-		// }
-
 		switch(*format)
 		{
 			case 'c':
